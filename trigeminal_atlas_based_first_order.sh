78a140d49451277d0805d8dc32b1b70045732ed5
arnaudbore <arnaud.bore@gmail.com>
2026-01-28 10:48:30 -0500

    fix tabs

--- trigeminal_atlas_based_first_order.sh
+++ trigeminal_atlas_based_first_order.sh
@@ -78,8 +78,8 @@ npv="400" #TODO: add an npv option. No need for npy=100. Sub-optimal. At npv 400
 
 opposite_side=leftright
 fa_threshold=0.1 # TODO: Maybe go with 0.15 as Nasrine. To test.  
-                 # TODO: "hard" we need a real map-include/map-exclude map to run PFT. The nerve is CSF.
-                 #      PFT would allow the tracking to bounce of the CSF to continue tracking
+# TODO: "hard" we need a real map-include/map-exclude map to run PFT. The nerve is CSF.
+#        PFT would allow the tracking to bounce of the CSF to continue tracking
 
 mkdir -p ${out_dir}/orig_space/{bundles_mask,transfo}
 mkdir -p ${out_dir}/{orig_space,mni_space}/rois
@@ -93,19 +93,19 @@ mni_tracking_dir=${out_dir}/mni_space/tractograms/
 
 echo "|------------- 1) Register mni_masked in orig space -------------|"
 antsRegistrationSyN.sh -d 3 \
-		       -f ${subject_dir}/tractoflow/*__t1_warped.nii.gz \
-		       -m ${mni_dir}/MNI/mni_masked.nii.gz \
-		       -t s \
-		       -o ${out_dir}/orig_space/transfo/2orig_ \
-		       -y 1 \
-		       -n ${nb_thread} >> ${out_dir}/orig_space/transfo/2orig_log.txt
+    -f ${subject_dir}/tractoflow/*__t1_warped.nii.gz \
+    -m ${mni_dir}/MNI/mni_masked.nii.gz \
+    -t s \
+    -o ${out_dir}/orig_space/transfo/2orig_ \
+    -y 1 \
+    -n ${nb_thread} >> ${out_dir}/orig_space/transfo/2orig_log.txt
 
 echo "|------------- 1.1) Reshape aparc.DKTatlas+aseg.mgz orig space -------------|"
 ## [ORIG-SPACE] Reshape aparc.DKTatlas+aseg.mgz
 scil_volume_reslice_to_reference ${subject_dir}/freesurfer/aparc.DKTatlas+aseg.mgz \
-				 ${subject_dir}/tractoflow/*__t1_warped.nii.gz \
-				 ${orig_rois_dir}/aparc.DKTatlas+aseg_orig.nii.gz \
-				 --interpolation nearest --keep_dtype -f
+    ${subject_dir}/tractoflow/*__t1_warped.nii.gz \
+    ${orig_rois_dir}/aparc.DKTatlas+aseg_orig.nii.gz \
+    --interpolation nearest --keep_dtype -f
 echo "|------------- 1) Done -------------|"
 echo ""
 
@@ -124,42 +124,42 @@ Any_Exclusion_ROI=(${Left_Cerebral_Cortex[*]} ${Right_Cerebral_Cortex[*]} ${Left
 ## Generate bilateral exclusion ROI
 echo "|------------- 2.1) any_exclusion_roi_orig -------------|"
 scil_labels_combine ${orig_rois_dir}/any_exclusion_roi_orig.nii.gz \
-		    --volume_ids ${orig_rois_dir}/aparc.DKTatlas+aseg_orig.nii.gz ${Any_Exclusion_ROI[*]} \
-		    --merge_groups -f
+    --volume_ids ${orig_rois_dir}/aparc.DKTatlas+aseg_orig.nii.gz ${Any_Exclusion_ROI[*]} \
+    --merge_groups -f
 
 echo "|------------- 2.2) cerebellum_wm_right_orig -------------|"
 ## WM Cerebellum Right
 scil_labels_combine ${orig_rois_dir}/right_cerebellum_wm_orig.nii.gz \
-		    --volume_ids ${orig_rois_dir}/aparc.DKTatlas+aseg_orig.nii.gz ${Right_Cerebellum_WM[*]} \
-		    --merge_groups -f
+    --volume_ids ${orig_rois_dir}/aparc.DKTatlas+aseg_orig.nii.gz ${Right_Cerebellum_WM[*]} \
+    --merge_groups -f
 
 ## WM Cerebellum Left
 echo "|------------- 2.3) cerebellum_wm_left_orig -------------|"
 scil_labels_combine ${orig_rois_dir}/left_cerebellum_wm_orig.nii.gz \
-		    --volume_ids ${orig_rois_dir}/aparc.DKTatlas+aseg_orig.nii.gz ${Left_Cerebellum_WM[*]} \
-		    --merge_groups -f
+    --volume_ids ${orig_rois_dir}/aparc.DKTatlas+aseg_orig.nii.gz ${Left_Cerebellum_WM[*]} \
+    --merge_groups -f
 
 # WM mask
 scil_volume_math lower_threshold ${subject_dir}/tractoflow/*__fa.nii.gz \
-		 ${fa_threshold} \
-		 ${orig_rois_dir}/wm_mask_${fa_threshold}_orig.nii.gz \
-		 --data_type uint8 -f
+	${fa_threshold} \
+	${orig_rois_dir}/wm_mask_${fa_threshold}_orig.nii.gz \
+    --data_type uint8 -f
 
 # Register these ROI in MNI space
 echo "|------------- 2.4) Registration in MNI space -------------|"
 for roi in right_cerebellum_wm left_cerebellum_wm any_exclusion_roi
 do
     antsApplyTransforms -d 3 \
-			-i ${orig_rois_dir}/${roi}_orig.nii.gz \
-			-r ${mni_dir}/MNI/mni_masked.nii.gz \
-			-t [${out_dir}/orig_space/transfo/2orig_0GenericAffine.mat, 1] \
-			-t ${out_dir}/orig_space/transfo/2orig_1InverseWarp.nii.gz \
-			-o ${mni_rois_dir}/${roi}_mni.nii.gz \
-			-n NearestNeighbor;
+		-i ${orig_rois_dir}/${roi}_orig.nii.gz \
+		-r ${mni_dir}/MNI/mni_masked.nii.gz \
+		-t [${out_dir}/orig_space/transfo/2orig_0GenericAffine.mat, 1] \
+		-t ${out_dir}/orig_space/transfo/2orig_1InverseWarp.nii.gz \
+		-o ${mni_rois_dir}/${roi}_mni.nii.gz \
+		-n NearestNeighbor;
     
     scil_volume_math convert \
-		     ${mni_rois_dir}/${roi}_mni.nii.gz \
-		     ${mni_rois_dir}/${roi}_mni.nii.gz --data_type int16 -f
+		${mni_rois_dir}/${roi}_mni.nii.gz \
+		${mni_rois_dir}/${roi}_mni.nii.gz --data_type int16 -f
 done
 echo "|------------- 2) Done -------------|"
 echo ""
@@ -174,17 +174,17 @@ do
         atlas_component=`basename "$component"`
         echo "|------------- 3.2) Component : ${atlas_component} -------------|"
         antsApplyTransforms -d 3 \
-			    -i ${component} \
-			    -r ${subject_dir}/tractoflow/*__t1_warped.nii.gz \
-			    -t ${out_dir}/orig_space/transfo/2orig_1Warp.nii.gz \
-			    -t ${out_dir}/orig_space/transfo/2orig_0GenericAffine.mat \
-			    -o ${out_dir}/orig_space/${mask_type}/${atlas_component} \
-			    -n NearestNeighbor;
+            -i ${component} \
+            -r ${subject_dir}/tractoflow/*__t1_warped.nii.gz \
+            -t ${out_dir}/orig_space/transfo/2orig_1Warp.nii.gz \
+            -t ${out_dir}/orig_space/transfo/2orig_0GenericAffine.mat \
+            -o ${out_dir}/orig_space/${mask_type}/${atlas_component} \
+            -n NearestNeighbor;
 	
         scil_volume_math convert \
-			 ${out_dir}/orig_space/${mask_type}/${atlas_component} \
-			 ${out_dir}/orig_space/${mask_type}/${atlas_component} \
-			 --data_type int16 -f;
+            ${out_dir}/orig_space/${mask_type}/${atlas_component} \
+            ${out_dir}/orig_space/${mask_type}/${atlas_component} \
+            --data_type int16 -f;
     done
 done
 echo "|------------- 3) Done -------------|"
@@ -203,7 +203,7 @@ do
         for theta in "${theta_list[@]}"; do
             combo_tag=step_${step_size}_theta_${theta}
             # TODO: mesecenphalic could use a bigger NPV > remaining_cp > spinal
-            echo "|------------- 4.1) Tracking from atlas component ${atlas_component} with npv=${npv} -------------|"
+            echo "|------------- 4.1) Tracking from atlas component ${atlas_component} with npv=${npv}, step=${step_size}, theta=${theta} -------------|"
 
             scil_tracking_local ${subject_dir}/tractoflow/*__fodf.nii.gz \
                 ${out_dir}/orig_space/bundles_mask/${atlas_component} \
@@ -218,15 +218,15 @@ do
     done
 done
 
-echo "|------------- 4) Tracking from atlas component  -------------|"
 for component in left_mesencephalic.nii.gz left_spinal.nii.gz left_remaining_cp.nii.gz right_mesencephalic.nii.gz right_spinal.nii.gz right_remaining_cp.nii.gz #${atlas_dir}/bundles_mask/*;
 do
-    scil_tractogram_math lazy_concatenate\
-        ${out_dir}/orig_space/tractograms/orig__*_${component/.nii.gz/.trk} \
-        ${out_dir}/orig_space/tractograms/orig__${component/.nii.gz/.trk} -f
-
     atlas_component=$component
-    echo "|------------- 4.2) Register tracking to MNI space -------------|"
+    echo "|------------- 4.2) Concatenate tracking results for atlas component ${component} -------------|"
+    scil_tractogram_math lazy_concatenate\
+        ${out_dir}/orig_space/tractograms/orig__*_${atlas_component/.nii.gz/.trk} \
+        ${out_dir}/orig_space/tractograms/orig__${atlas_component/.nii.gz/.trk} -f
+
+    echo "|------------- 4.3) Register tracking to MNI space -------------|"
     scil_tractogram_apply_transform \
         ${out_dir}/orig_space/tractograms/orig__${atlas_component/.nii.gz/.trk} \
         ${mni_dir}/MNI/mni_masked.nii.gz \
@@ -240,8 +240,6 @@ do
     mv ${out_dir}/orig_space/tractograms/orig_*trk ${out_dir}/orig_space/tractograms/orig/
 done
 
-
-
 echo "|------------- 4) Done -------------|"
 echo ""
 
@@ -254,12 +252,12 @@ do
     do
         echo "|------------- 5.1b) Major filtering for ${atlas_component} nside: ${nside} npv: ${npv} -------------|"
         scil_tractogram_filter_by_roi ${mni_tracking_dir}/orig__${nside}_${atlas_component}.trk \
-				      ${mni_tracking_dir}/filtered_${nside}_${atlas_component}.trk \
-				      --drawn_roi ${mni_rois_dir}/any_exclusion_roi_mni.nii.gz 'any' 'exclude' \
-				      --drawn_roi ${mni_rois_dir}/${nside/${nside}/${opposite_side/${nside}/}}_cerebellum_wm_mni.nii.gz 'any' 'exclude' \
-				      --drawn_roi ${mni_rois_dir}/${nside}_cerebellum_wm_mni.nii.gz 'either_end' 'exclude' \
-				      --drawn_roi ${mni_dir}/MNI/midsagittal_plane.nii.gz 'any' 'exclude' \
-				      --drawn_roi ${mni_dir}/MNI/cp_${nside}_bin.nii.gz any include -f -v # It was not needed when creating atlas since everything was tracked from cp
+            ${mni_tracking_dir}/filtered_${nside}_${atlas_component}.trk \
+            --drawn_roi ${mni_rois_dir}/any_exclusion_roi_mni.nii.gz 'any' 'exclude' \
+            --drawn_roi ${mni_rois_dir}/${nside/${nside}/${opposite_side/${nside}/}}_cerebellum_wm_mni.nii.gz 'any' 'exclude' \
+            --drawn_roi ${mni_rois_dir}/${nside}_cerebellum_wm_mni.nii.gz 'either_end' 'exclude' \
+            --drawn_roi ${mni_dir}/MNI/midsagittal_plane.nii.gz 'any' 'exclude' \
+            --drawn_roi ${mni_dir}/MNI/cp_${nside}_bin.nii.gz any include -f -v # It was not needed when creating atlas since everything was tracked from cp
     done
 done
 
@@ -268,11 +266,11 @@ echo "|------------- 5.2) Segmentation for mesencephalic -------------|"
 for nside in left right
 do
     scil_tractogram_filter_by_roi ${mni_tracking_dir}/filtered_${nside}_mesencephalic.trk \
-				  ${mni_tracking_dir}/segmented_${nside}_mesencephalic.trk  \
-				  --drawn_roi ${mni_dir}/MNI/upper_cut_brainstem.nii.gz 'any' 'include' \
-				  --drawn_roi ${mni_dir}/MNI/coronal_plane.nii.gz 'any' 'include' \
-				  --drawn_roi ${mni_dir}/MNI/coronal_plane_for_mesencephalic.nii.gz 'any' 'include' \
-				  -f
+        ${mni_tracking_dir}/segmented_${nside}_mesencephalic.trk  \
+        --drawn_roi ${mni_dir}/MNI/upper_cut_brainstem.nii.gz 'any' 'include' \
+        --drawn_roi ${mni_dir}/MNI/coronal_plane.nii.gz 'any' 'include' \
+        --drawn_roi ${mni_dir}/MNI/coronal_plane_for_mesencephalic.nii.gz 'any' 'include' \
+        -f
 
     # TODO: length threshold
     # 37 mm < length < 82 mm
@@ -287,12 +285,12 @@ echo "|------------- 5.3) Segmentation remaining cp -------------|"
 for nside in left right
 do
     scil_tractogram_filter_by_roi ${mni_tracking_dir}/filtered_${nside}_remaining_cp.trk \
-				  ${mni_tracking_dir}/segmented_${nside}_remaining_cp.trk  \
-				  --drawn_roi ${mni_dir}/MNI/lower_cut_brainstem.nii.gz 'any' 'exclude' \
-				  --drawn_roi ${mni_dir}/MNI/upper_cut_brainstem.nii.gz 'any' 'exclude' \
-				  --drawn_roi ${mni_dir}/MNI/coronal_plane.nii.gz 'any' 'include' \
-				  --bdo ${mni_dir}/MNI/sphere_exclusion_for_remaining_cp.bdo 'any' 'exclude' \
-				  -f
+        ${mni_tracking_dir}/segmented_${nside}_remaining_cp.trk  \
+        --drawn_roi ${mni_dir}/MNI/lower_cut_brainstem.nii.gz 'any' 'exclude' \
+        --drawn_roi ${mni_dir}/MNI/upper_cut_brainstem.nii.gz 'any' 'exclude' \
+        --drawn_roi ${mni_dir}/MNI/coronal_plane.nii.gz 'any' 'include' \
+        --bdo ${mni_dir}/MNI/sphere_exclusion_for_remaining_cp.bdo 'any' 'exclude' \
+        -f
     
     # TODO: length threshold
     # 9 mm < length < 38 mm
@@ -313,10 +311,10 @@ echo "|------------- 5.4) Segmentation spinal -------------|"
 for nside in left right
 do
     scil_tractogram_filter_by_roi ${mni_tracking_dir}/filtered_${nside}_spinal.trk \
-				  ${mni_tracking_dir}/segmented_${nside}_spinal.trk \
-				  --drawn_roi ${mni_dir}/MNI/lower_cut_brainstem.nii.gz 'any' 'include' \
-				  --drawn_roi ${mni_dir}/MNI/coronal_plane.nii.gz 'any' 'include' \
-				  -f
+        ${mni_tracking_dir}/segmented_${nside}_spinal.trk \
+        --drawn_roi ${mni_dir}/MNI/lower_cut_brainstem.nii.gz 'any' 'include' \
+        --drawn_roi ${mni_dir}/MNI/coronal_plane.nii.gz 'any' 'include' \
+        -f
 
     # TODO: we need better filetring with ROI of the spinal part. There is a branch that we pick that we should not have.
 
@@ -336,12 +334,12 @@ do
     for nside in left right
     do
         scil_tractogram_apply_transform ${mni_tracking_dir}/final_${nside}_${atlas_component}.trk \
-					${subject_dir}/tractoflow/*__t1_warped.nii.gz \
-					${out_dir}/orig_space/transfo/2orig_0GenericAffine.mat \
-					${orig_tracking_dir}/final/final_${nside}_${atlas_component}.trk \
-					--inverse \
-					--in_deformation ${out_dir}/orig_space/transfo/2orig_1InverseWarp.nii.gz \
-					--remove_invalid -f
+            ${subject_dir}/tractoflow/*__t1_warped.nii.gz \
+            ${out_dir}/orig_space/transfo/2orig_0GenericAffine.mat \
+            ${orig_tracking_dir}/final/final_${nside}_${atlas_component}.trk \
+            --inverse \
+            --in_deformation ${out_dir}/orig_space/transfo/2orig_1InverseWarp.nii.gz \
+            --remove_invalid -f
     done
 done
 
